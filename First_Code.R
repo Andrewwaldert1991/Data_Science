8cb31f0243fa47bee1f8cf4c02e488f9b61c62ce
Andrewwaldert <a.j.waldert@gmail.com>
2020-05-26 20:09:20 +0200

    Added Script for Attrition Dataset and Dataset

--- First_Code.R
+++ First_Code.R
@@ -30,10 +30,13 @@ m <- matrix(1:10, nrow = 2, ncol = 5, F) # by collumn
 dim(m) # gives the dimensions of m
 attributes(m) # gives the attributes of the matrix
 
-### Plotting a normal distribution
-x <- seq(from = -3, to = +3, length.out = 100)
-plot(x, dnorm(x))
-
+### Plotting a normal distribution with regions of interest marked on the distribution
+x <- seq(from = -3, to = +3, length.out = 100)#
+y <- dnorm(x)
+plot(x, y, main = "Normal Distribution", type = "l", ylab = "Density", xlab = "Quantile")
+abline(h=0)
+region.x <- x[1<= x & x <= 2]
+region.y <- y[1 <= x & x <= 2]
 
 
 m <- matrix(rnorm(100), nrow = 20, ncol = 5, T) # by row  of randomly generated numbers of normal distributiob
@@ -51,3 +54,32 @@ cbind(x,y) # binding the two arrays of numbers by the collumns
 rbind(x, y) # binding the two arrays of numbers by the rows 
 
 
+
+
+
+
+### Read in Attrition HR Dataset: 
+
+data <- read.csv("WA_Fn-UseC_-HR-Employee-Attrition.csv")
+
+
+### Split Dataset into attrition people and non attrition people 
+
+attrition_employees <- data[ which(data$Attrition == "Yes"), ] #Attrition Employees 
+Loyal_employees <- data[ which(data$Attrition == "No"), ] #Loyal Employees
+Salary_loyal_employees <- Loyal_employees["MonthlyIncome"] # Creating a variable, wich captures the monthly income of the loyal people
+Salary_attrition_employees <- attrition_employees["MonthlyIncome"] # Creating a variable, wich captures the monthly income of the attrition people
+companies_loyal_employees <- Loyal_employees["NumCompaniesWorked"]
+
+### Plotting the Loyal Employees 
+
+boxplot(Salary_loyal_employees, horizontal = T, main = "Salary of Loyal Employees")# Creating a boxplot 
+hist(Salary_loyal_employees, breaks = 10)
+
+### Creating DataFrame to Examine the relationship of loyal employees salary and numnber of companies worked for
+
+comparison_data_frame <- cbind(Salary_loyal_employees,companies_loyal_employees) # binding the two arrays of numbers by the collumns 
+
+### Loading tools: 
+library(ggplot2)
+library(tidr)
